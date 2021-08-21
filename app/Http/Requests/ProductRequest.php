7c56bd295533b4d7ff3f410c6f80596aa83643c6
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-21 23:57:42 +0900

    ルールの追加

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -24,7 +24,12 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'        => 'required|string|max:50',
+            'description' => 'required|string|max:500',
+            'image'       => 'required',
+            'price'       => 'required|integer|min:100|max:9999999',
+            'size'        => 'required|alpha_num',
+            'material'    => 'required|string',
         ];
     }
 }
