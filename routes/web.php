3c96785a12fbbfbbbcf72c6cd625350a6d7b6f67
akira-wd-1224 <jda.7seven.a24@gmail.com>
2021-08-27 21:08:29 +0900

    ルートの統合

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,26 @@ Route::name('cart.')
 
 
 
+Route::namespace('Admin')->group(function () {
+
+    // ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset'    => true,
+        'verify'   => false
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:admin')->group(function () {
+
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+
+    });
+
+});
+
+Route::resource('/', 'Admin\SellController');
 
 
 
