5cac6b1fd6c3c5f2add87703e65a4d41d0109d61
manequin118 <manequin118@gmail.com>
2021-09-08 23:24:42 +0900

    メモ追加

--- resources/views/user/product/index.blade.php
+++ resources/views/user/product/index.blade.php
@@ -19,8 +19,11 @@
             <div class="card">
                 <img src="{{ asset($product->image) }}" class="card-img">
                 <div class="card-body">
+                    <p class="card-image">{{ $product->image }}</p>
                     <p class="card-title">{{ $product->name }}</p>
                     <p class="card-text">¥{{ number_format($product->price) }}</p>
+                    <p class="card-text">{{ $product->size }}</p>
+                    {{-- gender_category,item_category,brand_category--}}
                 </div>
             </div>
         </a>
