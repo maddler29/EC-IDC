dfcd79db94cf2dd54a5e9e9d0126cf6d5795e77e
masa <menta.masa29.php@gmail.com>
2021-10-05 23:39:49 +0900

    スライドショーとセレクトボックスを作成

---
+++ resources/views/layouts/user/login.blade.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') |{{ config('app.name', 'Laravel') }}</title>
+
+
+    <!-- Bootstrap Font Awesome-->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+<div id="app">
+    {{--    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
+    <nav class="navbar navbar-expand-md navbar-light bg-light ">
+        <div class="container">
+            <a class="navbar-brand col-md-3" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex col-md-8">
+                {{--検索フォーム機能--}}
+                <form class="form-inline" method="GET" action="{{ route('user.product.index') }}">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <select class="custom-select" name="brand_category">
+                                <option value="">全て</option>
+                                @foreach ($brand_categories as $category)
+                                    <option value="gender:{{$category->id}}" class="font-weight-bold">{{$category->gender}}</option>
+                                    @foreach ($category->brand_categories as $brand)
+                                        <option value="brand:{{$brand->id}}">　{{$brand->brand_name}}</option>
+                                    @endforeach
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="input-group-prepend">
+                            <select class="custom-select" name="item_category">
+                                <option value="">全て</option>
+                                @foreach ($item_categories as $category)
+                                    <option value="gender:{{$category->id}}" class="font-weight-bold">{{$category->gender}}</option>
+                                    @foreach ($category->item_categories as $item)
+                                        <option value="item:{{$item->id}}">　{{$item->item_name}}</option>
+                                    @endforeach
+                                @endforeach
+                            </select>
+                        </div>
+                        <input type="text" name="keyword" class="form-control" aria-label="Text input with dropdown button" placeholder="キーワード検索">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-outline-dark">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    {{--userがGuard で 認証されたユーザーだけにアクセス許可していなければ、
+                    ログインしなければならない--}}
+                    @unless (Auth::guard('user')->check())
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.login') }}">{{ __('Login') }}</a>
+                        </li>
+                        {{-- 管理者で新規登録 --}}
+                        @if (Route::has('user.register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('user.register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name ?? '' }} <span class="caret"></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                {{--                                <a class="dropdown-item" href="{{ route('user.product.create') }}">商品を出品</a>--}}
+                                <a class="dropdown-item" href="{{ route('user.mypage.edit') }}">プロフィール編集</a>
+                                <a class="fas fa-shopping-cart dropdown-item" href="{{ route('user.cart.index') }}">
+                                    カート
+                                </a>
+                                <a class="dropdown-item" href="{{ route('user.inquiry.create') }}">
+                                    お問い合わせ
+                                </a>
+                                <a class="dropdown-item" href="{{ route('user.logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('user.logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endunless
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="py-4">
+    @yield('content')
+    </main>
+</div>
+<!-- Bootstrap Vue JavaScript -->
+<script src="{{ mix('js/app.js') }}"></script>
+<!-- MDBootstrap JavaScript -->
+<script type="text/javascript" src="/js/mdb.min.js"></script>
+
+@yield('script')
+</body>
+
+</html>
--- resources/views/user/product/index.blade.php
+++ resources/views/user/product/index.blade.php
@@ -1,48 +1,71 @@
-@extends('layouts.user.app')
+@extends('layouts.user.login')
 
 @section('title')
 商品一覧
 @endsection
 
 @section('content')
-<div class="jumbotron top-img">
-    <p class="text-center text-white top-img-text">{{ config('app.name', 'Laravel') }}</p>
-</div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block mx-auto" src="{{ asset('/images/slide/slide.jpg') }}" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block mx-auto" src="{{ asset('/images/slide/slide001.jpg') }}" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block mx-auto" src="{{ asset('/images/slide/slide002.jpg') }}" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
 <div class="container">
     <div class="top__title text-center">
         All Products
     </div>
     <div class="row">
-        @foreach( $products as $product )
+        @foreach( $items as $item )
         <div class="col-3 mb-3">
-            <a href="{{ route('user.product.show', $product->id) }}" class="bg-secondary  d-inline-block d-flex justify-content-center  flex-column" role="button" style="border-radius: 75px;">
+            <a href="{{ route('user.product.show', $item->id) }}" class="bg-secondary  d-inline-block d-flex justify-content-center  flex-column" role="button" style="border-radius: 75px;">
                 <div class="card ">
                     <div class="position-relative overflow-hidden">
-                        <img src=" /storage/avatars/{{$product->image}}" class="card-image">
+                        <img src=" /storage/avatars/{{$item->image}}" class="card-image">
                         <div class="position-absolute py-2 px-3" style="left: 0; bottom: 10px; color: white; background-color: rgba(0, 0, 0, 0.70)">
                             <i class="fas fa-yen-sign"></i>
-                            <span class="ml-1">{{number_format($product->price)}}</span>
+                            <span class="ml-1">{{number_format($item->price)}}</span>
                         </div>
 
-                        @if ($product->isStateBought)
+                        @if ($item->isStateBought)
                         <div class=" position-absolute py-1 font-weight-bold d-flex justify-content-center align-items-end" style="left: 0; top: 0; color: white; background-color: #EA352C; transform: translate(-50%,-50%) rotate(-45deg); width: 125px; height: 125px; font-size: 20px;">
                             <span>SOLD</span>
                         </div>
                         @endif
                     </div>
                     <div class="card-body">
-                        <h5>{{ $product->name }}</h5>
-                        <p class="text-muted">サイズ : 性別 {{ $product->size }} /
-                            @if(($product->item_categories->gender_id ) == 1)
+                        <h5>{{ $item->name }}</h5>
+                        <p class="text-muted">サイズ : 性別 {{ $item->size }} /
+                            @if(($item->item_categories->gender_id ) == 1)
                             Men's
                             @else
                             Wemen's
                             @endif
                         </p>
                         <p class="text-muted">ブランド : アイテム </br>
-                            {{ $product->brand_categories->brand_name }} /
-                            {{$product->item_categories->item_name }}
+                            {{ $item->brand_categories->brand_name }} /
+                            {{$item->item_categories->item_name }}
                         </p>
                     </div>
                 </div>
@@ -53,7 +76,7 @@
         @endforeach
     </div>
     <div class="d-flex justify-content-center">
-        {{ $products->links() }}
+        {{ $items->links() }}
     </div>
 </div>
 @endsection
